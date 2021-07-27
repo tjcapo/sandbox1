3d19bd104778000ef812b8192df31bb4ad991c8f
tjcapo <tj.capo@protonmail.com>
2021-07-27 19:50:09 +0000

    added program 7-8

---
+++ programming_in_c/ch_7/program_7-8.c
@@ -0,0 +1,36 @@
+/*  Page 166
+    1. Calculate the absolute value of a number
+    2. Calculate square root of a number*/
+    
+    
+#include <stdio.h>
+
+float absoluteValue (float x)
+{
+    if (x < 0)
+        x = -x;
+        
+    return x;
+}
+
+float squareRoot (float x)
+{
+    const float  epsilon = .00001;
+    
+    float  guess = 1.0;
+    
+    while (absoluteValue (guess * guess - x) >= epsilon)
+        guess = (x / guess + guess) / 2.0;
+        
+    return guess;
+}
+
+
+int main (void)
+{
+    printf ("squareRoot(2.0) = %f\n", squareRoot(2.0));
+    printf ("squareRoot(144.0 = %f\n", squareRoot(144.0));
+    printf ("squareRoot(17.5) = %f\n", squareRoot(17.5));
+    
+    return 0;
+}
