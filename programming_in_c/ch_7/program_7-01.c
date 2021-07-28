4291c52d1151f44bcb5fb5ea3189a7bcfa4bb91b
tjcapo <tj.capo@protonmail.com>
2021-07-28 01:21:41 +0000

    Put back lesson 7 files that I accidentally deleted

---
+++ programming_in_c/ch_7/program_7-01.c
@@ -0,0 +1,16 @@
+/*  Page 153 
+    Write a function  */
+
+#include <stdio.h>
+
+void printMessage (void)
+{
+    printf ("Programming is fun.\n");
+}
+
+int main (void)
+{
+    printMessage();
+
+    return 0;
+}
---
+++ programming_in_c/ch_7/program_7-03.c
@@ -0,0 +1,18 @@
+/*  Page 155
+    Calling functions  */
+
+#include <stdio.h>
+
+void printMessage (void)
+{
+    printf ("Programming is fun.\n");
+}
+
+
+int main (void)
+{
+    for (int i = 1; i <= 5; ++i)
+        printMessage();
+
+    return 0;
+}
